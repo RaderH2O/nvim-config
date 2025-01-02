c9db7c1a9e690c72bb4edb6da21b0fb3fa3c7e9d
RaderH2O <raderh2ogt@gmail.com>
2025-01-02 21:48:53 +0330

    Added some keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,5 +1,9 @@
 -- Place your keybindings here
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
 
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', 'L', '<Cmd>:bn<CR>')
 vim.keymap.set('n', 'H', '<Cmd>:bp<CR>')
+
+vim.keymap.set('n', '<leader>bp', '<Cmd>:BufferLineTogglePin<CR>', { desc = 'Pin the buffer' })
