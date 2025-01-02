54c0f922611e73e243d03198982d25f9c7f634a7
RaderH2O <raderh2ogt@gmail.com>
2025-01-02 22:06:40 +0330

    Added some BufferLine keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,7 +3,8 @@ vim.keymap.set('n', 'j', 'gj')
 vim.keymap.set('n', 'k', 'gk')
 
 vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set('n', 'L', '<Cmd>:bn<CR>')
-vim.keymap.set('n', 'H', '<Cmd>:bp<CR>')
+vim.keymap.set('n', 'L', '<Cmd>:BufferLineCycleNext<CR>')
+vim.keymap.set('n', 'H', '<Cmd>:BufferLineCyclePrev<CR>')
 
 vim.keymap.set('n', '<leader>bp', '<Cmd>:BufferLineTogglePin<CR>', { desc = 'Pin the buffer' })
+vim.keymap.set('n', '<leader>bd', '<Cmd>:bd!<CR>:bn<CR>', { desc = 'Delete the buffer' })
